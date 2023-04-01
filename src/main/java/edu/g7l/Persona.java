1479600bc767dce90a9221ebcff649bc6c0207ae
lzappani <lzappani91@gmail.com>
2023-04-01 10:47:51 -0300

    Cambios a clase Persona

--- src/main/java/edu/g7l/Persona.java
+++ src/main/java/edu/g7l/Persona.java
@@ -1,13 +1,17 @@
 package edu.g7l;
 
-import lombok.Getter;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class Persona {
-    @Getter
     private String nombre;
-    private List<Pronostico> pronosticoList;
+    private final List<Pronostico> pronosticoList;
+
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+        this.pronosticoList = new ArrayList<Pronostico>();
+    }
 
 
     public void addPronostico(Pronostico pronostico) {
